9861bbb2bfb20e3664ff0e7d16ca1d02b4e4fc6d
Alex Magill <alexmagill@macbook-air.local>
2026-03-05 12:32:10 +0000

    refactor

--- sketch.js
+++ sketch.js
@@ -2,50 +2,58 @@
 // When they are close enough to each other (defined by 'link') they connect.
 // There are two types of connection, straight and curvy.
 
-var balls=[];
-var NOB = 30; // NOB = Number of balls
-var link = document.documentElement.clientWidth / 8; // base interlinking measurement on screen width
-var parentElement = 'background'; // The element ID that we'll attach the canvas to.
+let balls = [];
+let NOB = 30; // NOB = Number of balls
+let link = 0; // set in setup + resize
+let parentElement = 'background'; // The element ID that we'll attach the canvas to.
 
 // Set the colour palette
-var backgroundColor = '#201F2C'; // Set the background colour - we use this to re-draw each frame so that they don't over-draw eachother.
-var ballColor = [249, 220, 92]; // Set the ball, and ball outlines (stroke) colour RGB
-var linkLineColor = [249, 220, 92]; // Set the linear link colour RGB
-var linkCurveColor = [49, 133, 252]; // Set the curve link colour RGB
+let backgroundColor = '#201F2C'; // Background colour (redraw each frame)
+let ballColor = [249, 220, 92]; // Ball + outlines (stroke) RGB
+let linkLineColor = [249, 220, 92]; // Linear link RGB
+let linkCurveColor = [49, 133, 252]; // Curve link RGB
 
-// Quick functuon to convert base RGB colours to p5.js's rgba color model
-// Inputs are: 
-// rgbColor =  an RGB array
+// Quick function to convert base RGB colours to p5.js rgba colour model
+// Inputs are:
+// rgbColor = an RGB array
 // alphaValue = RGBA alpha value from 0 to 1
 function setAlpha(rgbColor, alphaValue) {
-  var rgbOutput = rgbColor.slice(); // Copy rgbColor array
-  alphaValue = Math.round(255 * alphaValue); // Convert to nearest integer 
-  rgbOutput.push(alphaValue); // Build the output array
+  const rgbOutput = rgbColor.slice(); // copy array
+  const a = Math.round(255 * alphaValue);
+  rgbOutput.push(a);
   return rgbOutput;
-} 
+}
 
 function setup() {
   frameRate(30);
-  var myCanvas = createCanvas(windowWidth,windowHeight);
-  myCanvas.parent(parentElement); // set this up on the background element
-  for(var i=0; i<NOB; i++)
-  {
+
+  const myCanvas = createCanvas(windowWidth, windowHeight);
+  myCanvas.parent(parentElement);
+
+  link = windowWidth / 8;
+
+  balls = [];
+  for (let i = 0; i < NOB; i++) {
     balls.push(new Ball());
   }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  link = windowWidth / 8;
+}
+
 function draw() {
   background(backgroundColor);
-  for(var i=0; i<NOB; i++)
-  {
-    for(var j=0; j<NOB; j++){
-      if(i!=j){
-        balls[i].axconnect(balls[j]);
-        balls[i].exconnect(balls[j]);
-        balls[i].connect(balls[j]);
-        balls[i].reconnect(balls[j]);
-      }
+
+  // Only draw each pair once
+  for (let i = 0; i < NOB; i++) {
+    for (let j = i + 1; j < NOB; j++) {
+      balls[i].connectTo(balls[j]);
     }
+  }
+
+  for (let i = 0; i < NOB; i++) {
     balls[i].disp();
     balls[i].move();
   }
@@ -53,96 +61,118 @@ function draw() {
 
 function Ball() {
   this.d = 10;
-  this.xPos = random(this.d,width-this.d/2);
-  this.yPos = random(this.d,height-this.d/2);
-  this.xPosf = random(0.2,4);
-  this.yPosf = random(0.2,2);
-  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-  this.xPosPlus=random(this.d,width-this.d/2);
-  this.yPosPlus=random(this.d,height-this.d/2);
 
-  this.disp = function() {
+  this.xPos = random(this.d, width - this.d / 2);
+  this.yPos = random(this.d, height - this.d / 2);
+
+  this.xPosf = random(0.2, 4);
+  this.yPosf = random(0.2, 2);
+
+  // Separate direction per axis (less constrained movement)
+  this.xDir = Math.random() < 0.5 ? -1 : 1;
+  this.yDir = Math.random() < 0.5 ? -1 : 1;
+
+  // Control points for bezier (kept from your original approach)
+  this.xPosPlus = random(this.d, width - this.d / 2);
+  this.yPosPlus = random(this.d, height - this.d / 2);
+
+  this.disp = function () {
     fill(249, 220, 92);
     noStroke();
-    ellipse(this.xPos,this.yPos,this.d,this.d);
+    ellipse(this.xPos, this.yPos, this.d, this.d);
+
     stroke(setAlpha(ballColor, 0.6));
     noFill();
-    ellipse(this.xPos,this.yPos,this.d*2,this.d*2);
+    ellipse(this.xPos, this.yPos, this.d * 2, this.d * 2);
+
     stroke(setAlpha(ballColor, 0.3));
-    ellipse(this.xPos,this.yPos,this.d*4,this.d*4);
-    if (Math.abs(mouseX - this.xPos) < 45 && Math.abs(mouseY - this.yPos) < 45) 
-    {
+    ellipse(this.xPos, this.yPos, this.d * 4, this.d * 4);
+
+    // Circular mouse proximity checks (squared distance)
+    const mdx = mouseX - this.xPos;
+    const mdy = mouseY - this.yPos;
+    const md2 = mdx * mdx + mdy * mdy;
+
+    if (md2 < 45 * 45) {
       fill(setAlpha(ballColor, 0.1));
-      ellipse(this.xPos,this.yPos,this.d*4,this.d*4);
+      noStroke();
+      ellipse(this.xPos, this.yPos, this.d * 4, this.d * 4);
     }
-    if (Math.abs(mouseX - this.xPos) < 15 && Math.abs(mouseY - this.yPos) < 15) 
-    {
+    if (md2 < 15 * 15) {
       fill(setAlpha(ballColor, 0.2));
-      ellipse(this.xPos,this.yPos,this.d*4,this.d*4);
+      noStroke();
+      ellipse(this.xPos, this.yPos, this.d * 4, this.d * 4);
     }
-    if (Math.abs(mouseX - this.xPos) < 5 && Math.abs(mouseY - this.yPos) < 5) 
-    {
+    if (md2 < 5 * 5) {
       fill(setAlpha(ballColor, 0.3));
-      ellipse(this.xPos,this.yPos,this.d*4,this.d*4);
+      noStroke();
+      ellipse(this.xPos, this.yPos, this.d * 4, this.d * 4);
     }
   };
 
-  this.move = function() {
-    this.xPos += this.xPosf * plusOrMinus;
-    this.yPos += this.yPosf * plusOrMinus;
-    if(this.xPos > width-this.d/2) {
-      this.xPos = 0 + this.d/2;
-    } else if (this.xPos < this.d/2) {
-      this.xPos = width - this.d/2;
+  this.move = function () {
+    this.xPos += this.xPosf * this.xDir;
+    this.yPos += this.yPosf * this.yDir;
+
+    // Wrap-around (kept as in your original)
+    if (this.xPos > width - this.d / 2) {
+      this.xPos = 0 + this.d / 2;
+    } else if (this.xPos < this.d / 2) {
+      this.xPos = width - this.d / 2;
     }
-    if (this.yPos > height-this.d/2) {
-      this.yPos = 0 + this.d/2;
-    } else if (this.yPos < this.d/2) {
-      this.yPos = height - this.d/2;
+
+    if (this.yPos > height - this.d / 2) {
+      this.yPos = 0 + this.d / 2;
+    } else if (this.yPos < this.d / 2) {
+      this.yPos = height - this.d / 2;
     }
   };
 
-  this.connect = function(other) {
-    if (dist(this.xPos,this.yPos,other.xPos,other.yPos) < link / 2)
-    {
-      stroke(setAlpha(linkCurveColor, 0.2));
-      noFill();
-      beginShape();
-      vertex(this.xPos, this.yPos);
-      bezierVertex(this.xPosPlus, this.yPos, other.xPos, other.yPosPlus, other.xPos, other.yPos);
-      endShape();
-    }
-  };
+  // Unified connector: computes distance once (squared)
+  this.connectTo = function (other) {
+    const dx = this.xPos - other.xPos;
+    const dy = this.yPos - other.yPos;
+    const d2 = dx * dx + dy * dy;
 
-  this.exconnect = function(other) {
-    if (dist(this.xPos,this.yPos,other.xPos,other.yPos) < link / 1.5)
-    {
-      stroke(setAlpha(linkCurveColor, 0.1));
-      noFill();
-      beginShape();
-      vertex(this.xPos, this.yPos);
-      bezierVertex(this.xPosPlus, this.yPos, other.xPos, other.yPosPlus, other.xPos, other.yPos);
-      endShape();
-    }
-  };
+    const link2 = link * link;
 
-  this.axconnect = function(other) {
-    if (dist(this.xPos,this.yPos,other.xPos,other.yPos) < link)
-    {
-      stroke(setAlpha(linkCurveColor, 0.025));
-      noFill();
-      beginShape();
-      vertex(this.xPos, this.yPos);
-      bezierVertex(this.xPosPlus, this.yPos, other.xPos, other.yPosPlus, other.xPos, other.yPos);
-      endShape();
-    }
-  };
+    // Straight line further out (link * 1.5)
+    const lineThresh = link * 1.5;
+    const lineThresh2 = lineThresh * lineThresh;
 
-  this.reconnect = function(other) {
-    if (dist(this.xPos,this.yPos,other.xPos,other.yPos) < link * 1.5)
-    {
+    if (d2 < lineThresh2) {
       stroke(setAlpha(linkLineColor, 0.05));
-      line(this.xPos,this.yPos,other.xPos,other.yPos);
+      line(this.xPos, this.yPos, other.xPos, other.yPos);
+    }
+
+    // Curves nearer in, with stepped alpha similar to your 3 curve bands
+    // (link) -> 0.025, (link/1.5) -> 0.1, (link/2) -> 0.2
+    // Note: test in order from tightest to loosest so the strongest one wins.
+    const nearThresh = link / 2;
+    const midThresh = link / 1.5;
+    const farThresh = link;
+
+    const near2 = nearThresh * nearThresh;
+    const mid2 = midThresh * midThresh;
+    const far2 = farThresh * farThresh;
+
+    let curveAlpha = 0;
+
+    if (d2 < near2) curveAlpha = 0.2;
+    else if (d2 < mid2) curveAlpha = 0.1;
+    else if (d2 < far2) curveAlpha = 0.025;
+
+    if (curveAlpha > 0) {
+      stroke(setAlpha(linkCurveColor, curveAlpha));
+      noFill();
+      beginShape();
+      vertex(this.xPos, this.yPos);
+      bezierVertex(
+        this.xPosPlus, this.yPos,
+        other.xPos, other.yPosPlus,
+        other.xPos, other.yPos
+      );
+      endShape();
     }
   };
 }
